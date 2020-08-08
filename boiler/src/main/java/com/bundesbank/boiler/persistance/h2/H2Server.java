72feb8130bdee0e0b8ea474d55512524427ce622
georgy.saukov <georgy.saukov@wirecard.com>
2020-08-08 02:48:46 +0200

    H2

---
+++ boiler/src/main/java/com/bundesbank/boiler/persistance/h2/H2Server.java
@@ -0,0 +1,19 @@
+package com.bundesbank.boiler.persistance.h2;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+public class H2Server {
+
+//    new conn string jdbc:h2:tcp://localhost:9090/file:./data/db/spDb
+//    conn string jdbc:h2:tcp://localhost:9090/mem:spDb
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server inMemoryH2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+    }
+
+}
