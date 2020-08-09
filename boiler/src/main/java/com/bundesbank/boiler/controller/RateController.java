f91ccb64579d3d30c1a1ffe6d58bc20fb3f72263
georgy.saukov <georgy.saukov@wirecard.com>
2020-08-09 03:08:06 +0200

    controller.

---
+++ boiler/src/main/java/com/bundesbank/boiler/controller/RateController.java
@@ -0,0 +1,38 @@
+package com.bundesbank.boiler.controller;
+
+import com.bundesbank.boiler.persistance.entity.RateData;
+import com.bundesbank.boiler.persistance.repository.RateDataRepository;
+import com.bundesbank.boiler.service.load.BankCurrency;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api")
+public class RateController {
+
+    @Autowired
+    private RateDataRepository repository;
+
+    @GetMapping("/v1/rates")
+    public List<String> getCurrencies(){
+        return Stream.of(BankCurrency.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/v1/rates/{currencyId}")
+    public List<RateData> getRate(@PathVariable String currencyId){
+        return repository.findByCurrencyIdAndBbkObsStatus(BankCurrency.valueOf(currencyId), null);
+    }
+
+    @PostMapping("/v1/rates/{currencyId}/exchange")
+    public BigDecimal getRate(@PathVariable String currencyId, @RequestBody String date){
+        return new BigDecimal(10);
+    }
+
+}
--- boiler/src/main/java/com/bundesbank/boiler/persistance/repository/RateDataRepository.java
+++ boiler/src/main/java/com/bundesbank/boiler/persistance/repository/RateDataRepository.java
@@ -1,6 +1,7 @@
 package com.bundesbank.boiler.persistance.repository;
 
 import com.bundesbank.boiler.persistance.entity.RateData;
+import com.bundesbank.boiler.service.load.BankCurrency;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,8 +12,10 @@ import java.util.List;
 @Repository
 public interface RateDataRepository extends JpaRepository<RateData, String> {
 
-    @Query(value = "select rd.currencyId, max(rd.timePeriod) from RateData rd" +
-            " group by rd.currencyId")
+    List<RateData> findByCurrencyIdAndBbkObsStatus (BankCurrency currencyId, String bbkObsStatus);
+
+    @Query(value = "select rd.currencyId as currencyId, max(rd.timePeriod) as timePeriod " +
+            " from RateData rd group by rd.currencyId")
     List<LastUpdatedRateData> findLastUpdated ();
 
     interface LastUpdatedRateData {
