930484a713462d2c5e7e6b14b4d956613e7bdb78
georgy.saukov <georgy.saukov@wirecard.com>
2020-08-13 14:22:30 +0200

    rate query

--- boiler/src/main/java/com/bundesbank/boiler/controller/RateController.java
+++ boiler/src/main/java/com/bundesbank/boiler/controller/RateController.java
@@ -5,8 +5,10 @@ import com.bundesbank.boiler.persistance.repository.RateDataRepository;
 import com.bundesbank.boiler.service.ExchangeService;
 import com.bundesbank.boiler.service.load.BankCurrency;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -23,6 +25,12 @@ public class RateController {
     @GetMapping("/v1/rates/{currencyId}")
     public List<RateData> getRate(@PathVariable String currencyId){
         return repository.findByCurrencyIdAndBbkObsStatus(getBankCurrency(currencyId), null);
+    }
+
+    @GetMapping(value = "/v1/rates/{currencyId}/query", params = "date")
+    public RateData getRateQuery(@PathVariable String currencyId,
+                                 @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date){
+        return repository.findMostRecentToDate(currencyId, date);
     }
 
     @PostMapping("/v1/rates/{currencyId}/exchange")
